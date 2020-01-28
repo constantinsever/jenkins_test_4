b5e56169188784ebc2bcb7e9bc8da07ce0726627
Sever <constantinsever@gmail.com>
2020-01-28 11:50:44 +0100

    modificare la 12:49

---
+++ i1.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class i1 {
+
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println( "Hello at : " + sdf.format(cal.getTime()) );
+
+//add 1 12:49
+
+    }
+
+}
